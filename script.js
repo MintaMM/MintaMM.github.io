9334afcd2a385eee95302e0a4ddad8f98a88ebe6
MintaMM <92660044+MintaMM@users.noreply.github.com>
2025-05-19 21:59:38 +0800

    Update and rename script1.js to script.js

--- script.js
+++ script.js
@@ -48,18 +48,18 @@ noButton.addEventListener("click", function () {
     noButton.innerText = noTexts[clickCount - 1];
   }
   //改变背景颜色
-  if (clickCount === 1) body.style.backgroundColor = "#80d59b"； // 绿
-  if (clickCount === 2) body.style.backgroundColor = "#39aa5d"； // 
-  if (clickCount === 3) body.style.backgroundColor = "#1c552f"； // 
-  if (clickCount === 4) body.style.backgroundColor = "#012b17"； // 
-  if (clickCount >= 5) body.style.backgroundColor = "#012b17"； // 
+  if (clickCount === 1) body.style.backgroundColor = "#80d59b"; // 绿
+  if (clickCount === 2) body.style.backgroundColor = "#39aa5d"; // 
+  if (clickCount === 3) body.style.backgroundColor = "#1c552f"; // 
+  if (clickCount === 4) body.style.backgroundColor = "#012b17"; // 
+  if (clickCount >= 5) body.style.backgroundColor = "#012b17"; // 
 
   // 图片变化（前 5 次变化）
-  if (clickCount === 1) mainImage.src = "images/shocked.png"； // 震惊
-  if (clickCount === 2) mainImage.src = "images/think.png"； // 思考
-  if (clickCount === 3) mainImage.src = "images/angry.png"； // 生气
-  if (clickCount === 4) mainImage.src = "images/crying.png"； // 哭
-  if (clickCount >= 5) mainImage.src = "images/crying.png"； // 之后一直是哭
+  if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
+  if (clickCount === 2) mainImage.src = "images/think.png"; // 思考
+  if (clickCount === 3) mainImage.src = "images/angry.png"; // 生气
+  if (clickCount === 4) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount >= 5) mainImage.src = "images/crying.png"; // 之后一直是哭
 });
 
 // Yes 按钮点击后，进入表白成功页面
